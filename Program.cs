4e67cf35142303c7b2fb6222743e5e2efeeb43b9
Nikolay Kusch <dev.kusch@gmail.com>
2022-11-17 22:43:32 +0200

    Fix

--- Program.cs
+++ Program.cs
@@ -6,15 +6,28 @@ namespace DataClassConstructor
     {
         static void Main(string[] args)
         {
+            //reset out file
             File.WriteAllText("out.txt", "");
+
+            //open file with properties and read it
             var input = File.ReadAllText("input.txt").Split('\n');
-            foreach( var line in input)
+
+            foreach( var l in input)
             {
+                string line = l;
+
+                //remove the unnecessary end of the string 
+                while (line.Last().ToString() == " " || line.Last().ToString() == "\r" || line.Last().ToString() == "\n")
+                {
+                   line = line.Substring(0, line.Length - 1);
+                }
                 var item = line.Split(' ');
+
+                //check, is property does not has default value
                 if (item.Count() == 2)
                 {
-                    string type = item[0].Replace("\n", "").Replace("\r", "");
-                    string name = item[1].Replace("\n", "").Replace("\r", "");
+                    string type = item[0];
+                    string name = item[1];
                     string Name = name[0].ToString().ToUpper() + name.Substring(1);
                     string s =
 $"\n\nprivate {type} {name};" + "\n" +
@@ -30,12 +43,14 @@ $"        OnPropertyChanged(nameof({Name}));\n" +
 
                     File.AppendAllText("out.txt", s);
                 }
+
+                //check, is property has default value
                 if (item.Count() == 3)
                 {
-                    string type = item[0].Replace("\n", "").Replace("\r", "");
-                    string name = item[1].Replace("\n", "").Replace("\r", "");
+                    string type = item[0];
+                    string name = item[1];
                     string Name = name[0].ToString().ToUpper() + name.Substring(1);
-                    string value = item[2].Replace("\n", "").Replace("\r", "");
+                    string value = item[2];
                     string s =
 $"\n\nprivate {type} {name} = {value};" + "\n" +
 $"public {type} {Name}\n" +
@@ -47,6 +62,7 @@ $"        {name} = value;\n" +
 $"        OnPropertyChanged(nameof({Name}));\n" +
 "    }\n" +
 "}";
+                    //write property to out file
                     File.AppendAllText("out.txt", s);
                 }
             }
